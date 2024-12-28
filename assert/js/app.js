70e04ac9446e43a042956ecb99c6caf57b97bc41
unknown <zz240614@gmail.com>
2024-12-28 18:11:11 +0100

    SB-4 Ajouter functionalite drag start les taches

--- assert/js/app.js
+++ assert/js/app.js
@@ -5,6 +5,21 @@ let scrums = document.querySelectorAll('.Scrum');
 btn.onclick = function(){
     if (input.value != ''){
         scrums[0].innerHTML +=
-        `<p class = "tache" >${input.value}</p>`
+        `<p class = "tache" draggable="true" >${input.value}</p>`
     }
+    dragtache();
 }
+
+function dragtache(){
+    let taches = document.querySelectorAll('.tache');
+    taches.forEach(tache => {
+
+        tache.addEventListener('dragstart', function (e) {
+        //   drag = tache;
+          console.log('dragstar');
+        
+        })
+
+}
+    )};
+
