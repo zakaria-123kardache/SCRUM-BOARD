931c89ed7b195633746424baad29430acb7fed60
unknown <zz240614@gmail.com>
2024-12-28 18:19:49 +0100

    SB-4 Ajouter functionalite event  drag end  les taches

--- assert/js/app.js
+++ assert/js/app.js
@@ -9,14 +9,24 @@ btn.onclick = function(){
     }
     dragtache();
 }
+let drag = null; 
 
 function dragtache(){
     let taches = document.querySelectorAll('.tache');
     taches.forEach(tache => {
 
         tache.addEventListener('dragstart', function (e) {
-        //   drag = tache;
+          drag = tache;
+          tache.computedStyleMap.opacity = '0.5';
+          
           console.log('dragstar');
+          
+        })
+        
+        tache.addEventListener('dragend', function (e) {
+            drag = null;
+            console.log('dragend');
+            tache.computedStyleMap.opacity = '1';
         
         })
 
